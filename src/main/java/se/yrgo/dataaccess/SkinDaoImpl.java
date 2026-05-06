b68bbaf54f1c81085c0721f75c3259a28325f70b
Sebbe <sebban952@gmail.com>
2026-05-06 14:58:41 +0200

    added skindao and skindaoimpl

---
+++ src/main/java/se/yrgo/dataaccess/SkinDaoImpl.java
@@ -0,0 +1,28 @@
+package se.yrgo.dataaccess;
+
+import java.util.List;
+
+import se.yrgo.domain.Skin;
+
+public class SkinDaoImpl implements SkinDao {
+
+    @Override
+    public void addSkin(Skin skin) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addSkin'");
+    }
+
+    @Override
+    public Skin findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public List<Skin> findAvailableSkins() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAvailableSkins'");
+    }
+
+    
+}
